0c6f299dd613aa101c3aee424bab66e85c62c1ae
Calle Bokedal <calle.bokedal@teliacompany.com>
2020-04-15 00:14:31 +0200

    Added forgotten changed

--- app.js
+++ app.js
@@ -8,64 +8,101 @@ moment().format()
 //let now = moment();
 //let today = now.format('YYYY-MM-DD');
 
+const version = "1.0";
 
-// Default settings (with my secret, whoops!)
+// Default settings 
 const defaultSettings = {
-  //"deviceId": Math.random().toString(36).substr(2, 9),
+  "version": version, 
   "competitionDayLimit": 7, 
-  "favoriteOrganizors": ["Sjövalla FK"] //,
-  //"updateInterval": 30,
-  //"timeZone": "Europe/Stockholm"
+  "competitionCacheTTL": 120, 
+  "favoriteOrganizors": ["Sjövalla FK"] 
 }
 
 let competitions;
 let classes;
 
 getCompetitions = () => {
-  console.log("getCompetitions");
+  debug("getCompetitions");
 
+  let settings = loadSettings(defaultSettings)
 
+  let cachedCompetitions = getCompetitionsListCache(settings)
+  if(Object.keys(cachedCompetitions).length > 0) {
+    // Return cahced list
+    console.log("Return cached competitions")
+    generateCompetitionsList(cachedCompetitions)
+  } else {
+    // Fetch new data
 
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", "https://liveresultat.orientering.se/api.php?method=getcompetitions");
-  xhr.send(null);
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://liveresultat.orientering.se/api.php?method=getcompetitions");
+    xhr.send(null);
 
-  xhr.addEventListener("loadend", function() {
-    var json = JSON.parse(xhr.response);
-    if (xhr.status === 401) {
-      console.log("Can't access competitions")
-    } else if (xhr.status === 200 && json.competitions) {
-      competitions = json.competitions;
-      console.log(competitions[0])
+    xhr.addEventListener("loadend", function() {
+      var json = JSON.parse(xhr.response);
+      if (xhr.status === 401) {
+        console.log("Can't access competitions")
+      } else if (xhr.status === 200 && json.competitions) {
+        competitions = json.competitions;
+        //debug(competitions[0])
 
-      // Get settings
-      let settings = loadSettings(defaultSettings)
-      let filterDays = 7;
-      if(Number.isInteger(settings.competitionDayLimit)) {
-        filterDays = settings.competitionDayLimit
+        // Get settings
+        let filterDays = 7;
+        if(Number.isInteger(settings.competitionDayLimit)) {
+          filterDays = settings.competitionDayLimit
+        }
+        let favoriteOrganizors = []
+        if(Array.isArray(settings.favoriteOrganizors) && settings.favoriteOrganizors.length > 0) {
+          favoriteOrganizors = settings.favoriteOrganizors
+        }
+
+        // Filter by day and organizors
+        let filtered = competitions.filter( (c,i,ary) => {
+          let d = moment().diff(c.date, 'days');
+          let dayInScope = d >= 0 && d <= filterDays && c.timediff == 0;
+          let organizerInScope = favoriteOrganizors.length == 0 || favoriteOrganizors.indexOf(c.organizer) >= 0
+          return dayInScope && organizerInScope
+        });
+
+        debug(filtered)
+
+        saveCompetitionsListCache(filtered)
+        generateCompetitionsList(filtered)
+      } else {
+        console.log("No response!")
       }
-      let favoriteOrganizors = []
-      if(Array.isArray(settings.favoriteOrganizors) && settings.favoriteOrganizors.length > 0) {
-        favoriteOrganizors = settings.favoriteOrganizors
-      }
-
-      // Filter by day and organizors
-      let filtered = competitions.filter( (c,i,ary) => {
-        let d = moment().diff(c.date, 'days');
-        let dayInScope = d >= 0 && d <= filterDays && c.timediff == 0;
-        let organizerInScope = favoriteOrganizors.length == 0 || favoriteOrganizors.indexOf(c.organizer) >= 0
-        return dayInScope && organizerInScope
-      });
-
-      generateCompetitionsList(filtered)
-    } else {
-      console.log("No response!")
-    }
-  });
+    });
+  }
 };
 
-generateCompetitionsList = (data) => {
+debug = (str) => {
+  console.log(str)
+}
 
+saveCompetitionsListCache = (data) => {
+  let now = moment();
+  debug("saveCompetitionsListCache ")
+  localStorage.setItem("cachedCompetitionsTimestamp", JSON.stringify(now));
+  localStorage.setItem("cachedCompetitions", JSON.stringify(data));
+}
+
+getCompetitionsListCache = (settings) => {
+  let cachedCompetitionsTimestamp = localStorage.getItem("cachedCompetitionsTimestamp") ? JSON.parse(localStorage.getItem("cachedCompetitionsTimestamp")) : moment()
+
+  let secondsSinceLastFetch = moment().diff(cachedCompetitionsTimestamp,'seconds');
+  debug("secondsSinceLastFetch: " + secondsSinceLastFetch)
+
+  let cachedCompetitions = {}
+  debug("settings.competitionCacheTTL: " + settings.competitionCacheTTL)
+  if(secondsSinceLastFetch <= settings.competitionCacheTTL) {
+    // Return cached list
+    cachedCompetitions = localStorage.getItem("cachedCompetitions") ? JSON.parse(localStorage.getItem("cachedCompetitions")) : {};
+    
+  }
+  return cachedCompetitions
+}
+
+generateCompetitionsList = (data) => {
   if(data.length > 0) {
     let html = ""
 
@@ -92,54 +129,38 @@ generateCompetitionsList = (data) => {
   }
 }
 
-
 getClasses = () => {
-  console.log("get classes")
+  debug("get classes")
 }
 
 filterClasses = (id) => {
-  console.log("filter classes - " + id) 
+  debug("filter classes - " + id) 
 }
 
 loadSettings = (defaultSettings) => {
   console.log("loadSettings");
-  var settings = localStorage.getItem("settings") ? JSON.parse(localStorage.getItem("settings")) : defaultSettings;
+  let settings = localStorage.getItem("settings") ? JSON.parse(localStorage.getItem("settings")) : defaultSettings;
+  if(settings.version !== version) {
+    debug("Version diff: " + settings.version + " is not: " + version)
+  }
+  //localStorage.setItem("settings", JSON.stringify(settings));
 
-  localStorage.setItem("settings", JSON.stringify(settings));
+  //let cachedCompetitions = localStorage.getItem("cachedCompetitions") ? JSON.parse(localStorage.getItem("cachedCompetitions")) : {};
+  //localStorage.setItem("cachedCompetitions", JSON.stringify(cachedCompetitions));
+
   return settings;
 }
 
 saveSettings = (defaultSettings) => {
-  console.log("load settings")
+  debug("load settings")
 }
 
-
-/* EVENT LISTENERS */
+// EVENT LISTENERS 
 $( document ).ready(function() {
-  console.log( "ready!" );
+  debug( "ready!" );
   // Classes dropdown
   //$('.dropdown-toggle').dropdown()
 
   // Get recent competitions
   getCompetitions()
-});
-document.addEventListener("DOMContentLoaded", function() {
-
-
-
-  // save
-//  document.getElementById("settings-form").addEventListener("submit", function(e){
-//    e.preventDefault();
-//    saveSettings(defaultSettings);
-//    if (settings = loadSettings()) {
-//      showElement(document.getElementById("table"));
-//      getDepartures(null);
-//    }
-//  });
-
-  //getCompetitions()
-
-  // go!
-  //if (settings = loadSettings(defaultSettings)) getAccessToken(getDepartures(null));
-  //if (!settings.skipWelcome) showElement(document.getElementById("welcome"));
 });
