dac3d85d0d90a7ca96a328ccba1ae98881b05635
Calle Bokedal <calle.bokedal@teliacompany.com>
2020-04-14 20:06:29 +0200

    UI adjustments and intro of settings

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 // Init moment
 moment().format()
-moment.locale('sv');
+// console.log(moment.locale())
+// moment.locale("x-pseudo");
+// console.log(moment.locale())
+// moment.updateLocale();
+// console.log(moment.locale())
 //let now = moment();
 //let today = now.format('YYYY-MM-DD');
 
 
 // Default settings (with my secret, whoops!)
 const defaultSettings = {
-  "deviceId": Math.random().toString(36).substr(2, 9),
-  "stopName": "Centralstationen, Göteborg",
-  "updateInterval": 30,
-  "timeZone": "Europe/Stockholm"
+  //"deviceId": Math.random().toString(36).substr(2, 9),
+  "competitionDayLimit": 7, 
+  "favoriteOrganizors": ["Sjövalla FK"] //,
+  //"updateInterval": 30,
+  //"timeZone": "Europe/Stockholm"
 }
 
 let competitions;
@@ -18,28 +23,41 @@ let classes;
 
 getCompetitions = () => {
   console.log("getCompetitions");
+
+
+
   var xhr = new XMLHttpRequest();
   xhr.open("GET", "https://liveresultat.orientering.se/api.php?method=getcompetitions");
-  //xhr.setRequestHeader("Authorization", "Bearer " + localStorage.getItem("accessToken"));
   xhr.send(null);
 
   xhr.addEventListener("loadend", function() {
     var json = JSON.parse(xhr.response);
     if (xhr.status === 401) {
-      //getAccessToken(getStops);
+      console.log("Can't access competitions")
     } else if (xhr.status === 200 && json.competitions) {
-      //generateDataList(json.LocationList.StopLocation);
       competitions = json.competitions;
-      //console.log(competitions)
       console.log(competitions[0])
-      let local = competitions.filter( (c,i,ary) => {
-        //console.log(today)
-        let d = moment().diff(c.date, 'days')
-        return d >= 0 && d <= 7 && c.timediff == 0
-        //return c.timediff == 0
+
+      // Get settings
+      let settings = loadSettings(defaultSettings)
+      let filterDays = 7;
+      if(Number.isInteger(settings.competitionDayLimit)) {
+        filterDays = settings.competitionDayLimit
+      }
+      let favoriteOrganizors = []
+      if(Array.isArray(settings.favoriteOrganizors) && settings.favoriteOrganizors.length > 0) {
+        favoriteOrganizors = settings.favoriteOrganizors
+      }
+
+      // Filter by day and organizors
+      let filtered = competitions.filter( (c,i,ary) => {
+        let d = moment().diff(c.date, 'days');
+        let dayInScope = d >= 0 && d <= filterDays && c.timediff == 0;
+        let organizerInScope = favoriteOrganizors.length == 0 || favoriteOrganizors.indexOf(c.organizer) >= 0
+        return dayInScope && organizerInScope
       });
-      //console.log(local)
-      generateCompetitionsList(local)
+
+      generateCompetitionsList(filtered)
     } else {
       console.log("No response!")
     }
@@ -47,29 +65,35 @@ getCompetitions = () => {
 };
 
 generateCompetitionsList = (data) => {
-  let html = ""
 
-  data.forEach(data => {
-    //console.log(comp)
-    html += "<li class='list-group-item'><span class='date'>" + data.date + "</span> <a href=#" + data.id + " class='name'>" + data.name + "</a> (<span class=''>" + data.organizer + "</span>)</li>"
-  });
+  if(data.length > 0) {
+    let html = ""
 
-  document.getElementById("competitions").innerHTML = html;
-  //<li class="list-group-item">Cras justo odio</li>
+    data.forEach(data => {
+      let d = moment().diff(data.date, 'days');
+      let badgeClass = "badge badge-white bg-white"
+      if(d === 0) {
+        badgeClass = "badge badge-dark"
+      }
 
+      html += '<li class="list-group-item bg-light">'
+        html += '<div class="d-flex justify-content-between">'
+          html += '<small class="' + badgeClass + '">' + data.date + '</small>'
+          html += '<small class="">' + data.organizer + '</small>'  
+        html += '</div>'
+        html += '<h6 class="d-flex align-items-end flex-column mb-1 mt-1"><a href="#"' + data.id + ' class="name">' + data.name + '</a></h5>'
+      html += '</li>'
 
-/*  <ul class="list-group">
-  <li class="list-group-item">Cras justo odio</li>
-  <li class="list-group-item">Dapibus ac facilisis in</li>
-  <li class="list-group-item">Morbi leo risus</li>
-  <li class="list-group-item">Porta ac consectetur ac</li>
-  <li class="list-group-item">Vestibulum at eros</li>
-</ul>*/
+    });
+
+    document.getElementById("competitions").innerHTML = html;
+  } else {
+    document.getElementById("competitions").innerHTML = "<li class='list-group-item'>Inga tävlingar att visa</li>"  ;
+  }
 }
 
 
 getClasses = () => {
-
   console.log("get classes")
 }
 
@@ -92,10 +116,11 @@ saveSettings = (defaultSettings) => {
 
 /* EVENT LISTENERS */
 $( document ).ready(function() {
-    console.log( "ready!" );
+  console.log( "ready!" );
   // Classes dropdown
-  $('.dropdown-toggle').dropdown()
+  //$('.dropdown-toggle').dropdown()
 
+  // Get recent competitions
   getCompetitions()
 });
 document.addEventListener("DOMContentLoaded", function() {
